7975071e6a2dff13d33573f0e871383f82d86baa
Luan Nico <luanpotter27@gmail.com>
2018-08-25 22:39:11 -0300

    Very basic plugin structure

--- android/src/main/java/xyz/luan/games/play/playgames/PlayGamesPlugin.java
+++ android/src/main/java/xyz/luan/games/play/playgames/PlayGamesPlugin.java
@@ -5,21 +5,76 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 
-/** PlayGamesPlugin */
-public class PlayGamesPlugin implements MethodCallHandler {
-  /** Plugin registration. */
+import android.content.Intent;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
+public class PlayGamesPlugin implements MethodCallHandler, ActivityResultListener {
+
+  private static final int RC_SIGN_IN = 1;
+
+  private Registrar registrar;
+  private PendingOperation pendingOperation;
+
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "play_games");
-    channel.setMethodCallHandler(new PlayGamesPlugin());
+    channel.setMethodCallHandler(new PlayGamesPlugin(registrar));
+  }
+
+  public PlayGamesPlugin(Registrar registrar) {
+    this.registrar = registrar;
+    this.registrar.addActivityResultListener(this);
+  }
+
+  private static class PendingOperation {
+    MethodCall call;
+    Result result;
+
+    PendingOperation(MethodCall call, Result result) {
+      this.call = call;
+      this.result = result;
+    }
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (pendingOperation != null) {
+      throw new IllegalStateException("Cannot be used concurrently!");
+    }
+    pendingOperation = new PendingOperation(call, result);
+    if (call.method.equals("signIn")) {
+      GoogleSignInOptions opts = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build();
+      GoogleSignInClient signInClient = GoogleSignIn.getClient(registrar.activity(), opts);
+      Intent intent = signInClient.getSignInIntent();
+      registrar.activity().startActivityForResult(intent, RC_SIGN_IN);
     } else {
+      pendingOperation = null;
       result.notImplemented();
     }
+  }
+
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (pendingOperation == null || requestCode != RC_SIGN_IN) {
+      return false;
+    }
+    GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+    if (result.isSuccess()) {
+        GoogleSignInAccount signedInAccount = result.getSignInAccount();
+        pendingOperation.result.success("Success: " + signedInAccount.getEmail());
+    } else {
+        String message = result.getStatus().getStatusMessage();
+        if (message == null || message.isEmpty()) {
+            message = "Unexpected error " + result.getStatus();
+        }
+        pendingOperation.result.success("Error: " + message);
+    }
+    return true;
   }
 }
--- lib/play_games.dart
+++ lib/play_games.dart
@@ -2,12 +2,31 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+enum SigninResultType {
+  SUCCESS,
+  ERROR,
+  IOS
+}
+
+class SigninResult {
+  SigninResultType type;
+  Account account;
+  String messages;
+}
+
+class Account {
+  String email;
+}
+
 class PlayGames {
   static const MethodChannel _channel =
       const MethodChannel('play_games');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<SigninResult> signIn() async {
+    final String result = await _channel.invokeMethod('signIn');
+    print('----------------');
+    print(result);
+    print('----------------');
+    return null;
   }
 }
